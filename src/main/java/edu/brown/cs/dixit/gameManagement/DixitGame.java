d33810f7daa1e48b04271f0bbd82973f06c29e9c
박상하 <sangha@bagsanghaui-MacBook-Air.local>
2017-04-23 17:38:52 -0400

    dixit game

--- src/main/java/edu/brown/cs/dixit/gameManagement/DixitGame.java
+++ src/main/java/edu/brown/cs/dixit/gameManagement/DixitGame.java
@@ -5,6 +5,9 @@ public class DixitGame implements MultiplayerGame{
 	private final int id;
 	private final int num_players;
 	
+	//wrapper for all the information 
+	//needs to contain the players & deck & referee & turn
+	
 	public DixitGame(int ID, int player_number) {
 		id = ID;
 		num_players = player_number;
--- src/main/java/edu/brown/cs/dixit/setting/Board.java
+++
@@ -1,6 +0,0 @@
-package edu.brown.cs.dixit.setting;
-
-public class Board {
-  //wrapper for all the information 
-  //contains the players & deck & referee & turn
-} 
